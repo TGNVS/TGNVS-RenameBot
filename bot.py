c19019e4dbc499208476240ef754ddf452ec1e1c
nvs drive <nvsdrive19@yahoo.com>
2023-07-12 19:53:27 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,11 @@ class Bot(Client):
         me = await self.get_me()
         self.mention = me.mention
         self.username = me.username  
-        self.uptime = Config.BOT_UPTIME     
+        self.uptime = Config.BOT_UPTIME
+        if Config.WEBHOOK:
+            app = web.AppRunner(await web_server())
+            await app.setup()       
+            await web.TCPSite(app, "0.0.0.0", 8080).start()     
         print(f"{me.first_name} Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")
         for id in Config.ADMIN:
             try: await self.send_message(id, f"{me.first_name}  Iꜱ Sᴛᴀʀᴛᴇᴅ.....✨️")                                
