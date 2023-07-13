90e80433a098ea4e779b94d429a7829b5cd45ac0
nvs drive <nvsdrive19@yahoo.com>
2023-07-13 13:34:14 +0000

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -103,6 +103,16 @@ def shorten_url(url):
     return str(requests.get(site_url).text)
     #return site_url
 
+async def get_size(size):
+    """Get size in readable format"""
+    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+    size = float(size)
+    i = 0
+    while size >= 1024.0 and i < len(units):
+        i += 1
+        size /= 1024.0
+    return "%.2f %s" % (size, units[i])
+
 async def send_log(b, u):
     if Config.LOG_CHANNEL is not None:
         curr = datetime.now(timezone("Asia/Kolkata"))
